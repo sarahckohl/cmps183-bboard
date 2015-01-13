a5922529f02aa643f7ef8c5b7665274d9f60b1e3
Luca de Alfaro <luca@dealfaro.org>
2015-01-13 09:30:47 -0800

    We can add posts.

--- controllers/default.py
+++ controllers/default.py
@@ -17,6 +17,16 @@ def index():
     posts = db().select(db.bboard.ALL)
     return dict(posts=posts)
 
+def add():
+    """Add a post."""
+    form = SQLFORM(db.bboard)
+    if form.process().accepted:
+        # Successful processing.
+        session.flash = T('Added')
+        redirect(URL('default', 'index'))
+    return dict(form=form)
+
+
 
 def user():
     """
